48fc190d95eb6b255d83c69a2e913753b27d55cd
kotek <scpktk@gmail.com>
2025-09-02 14:27:36 +0900

    設定変更

--- packages/frontend/vitest.config.ts
+++ packages/frontend/vitest.config.ts
@@ -1,6 +1,13 @@
 import { defineConfig } from "vitest/config";
+import path from "path";
+
 export default defineConfig({
   test: {
     includeSource: ["src/**/*.ts", "test/**/*.ts"],
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
