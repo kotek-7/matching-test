159e1c52e3a5e99cc58869bfb1a94160839ec289
kotek <scpktk@gmail.com>
2025-09-08 09:10:05 +0900

    フロントの整備

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,9 +1,38 @@
 import { Hono } from "hono";
+import { upgradeWebSocket } from "hono/cloudflare-workers";
+import { cors } from "hono/cors";
 
 const app = new Hono();
 
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
-});
+app.use(
+  cors({
+    origin: "*",
+  }),
+);
+
+const route = app
+  .get("/", (c) => {
+    const value = Math.random();
+    return c.json({
+      ok: true,
+      message: `Hello, Hono! ${value}`,
+    });
+  })
+  .get(
+    "/ws",
+    upgradeWebSocket((c) => {
+      return {
+        onMessage(event, ws) {
+          console.log("WebSocket message received:", event.data);
+          const value = Math.random();
+          ws.send(`Hello from the server! ${value}`);
+        },
+        onClose(ws) {
+          console.log("WebSocket connection closed:", ws.reason);
+        },
+      };
+    }),
+  );
 
 export default app;
+export type AppType = typeof route;
--- packages/frontend/src/app/page.tsx
+++ packages/frontend/src/app/page.tsx
@@ -1,71 +1,59 @@
-import Image from "next/image";
-import React from "react";
+"use client";
+
+import { AppType } from "backend/src/index";
+import { hc } from "hono/client";
+import React, { useEffect, useRef, useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image className="dark:invert" src="/next.svg" alt="Next.js logo" width={180} height={38} priority />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">Save and see your changes instantly.</li>
-        </ol>
+  const clientRef = useRef(hc<AppType>("http://localhost:8787"));
+  const socketRef = useRef<WebSocket | null>(null);
+  const [value, setValue] = useState("");
+  const [wsValue, setWsValue] = useState("");
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image className="dark:invert" src="/vercel.svg" alt="Vercel logomark" width={20} height={20} />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image aria-hidden src="/file.svg" alt="File icon" width={16} height={16} />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image aria-hidden src="/window.svg" alt="Window icon" width={16} height={16} />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image aria-hidden src="/globe.svg" alt="Globe icon" width={16} height={16} />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  const handleClick = async () => {
+    const res = await clientRef.current.index.$get();
+    if (!res.ok) return;
+    const data = await res.json();
+    setValue(data.message);
+  };
+
+  useEffect(() => {
+    socketRef.current = clientRef.current.ws.$ws();
+    const socket = socketRef.current;
+    let intervalId: number;
+
+    const handleOpen = (_event: Event) => {
+      console.log("WebSocket is open now.");
+      intervalId = window.setInterval(() => {
+        socket.send("Hello from the client!");
+      }, 1000);
+    };
+    socket.addEventListener("open", handleOpen);
+
+    const handleMessage = (event: MessageEvent) => {
+      console.log("WebSocket message received:", event.data);
+      setWsValue(event.data);
+    };
+    socket.addEventListener("message", handleMessage);
+
+    return () => {
+      socket.removeEventListener("open", handleOpen);
+      socket.removeEventListener("message", handleMessage);
+      if (intervalId) {
+        clearInterval(intervalId);
+      }
+      socket.close();
+    };
+  }, []);
+
+  return (
+    <div>
+      <h1>WebSocket Matching Test</h1>
+      <p>{value}</p>
+      <p>{wsValue}</p>
+      <button onClick={handleClick} className="bg-blue-500 text-white px-4 py-2 rounded">
+        Fetch
+      </button>
     </div>
   );
 }
